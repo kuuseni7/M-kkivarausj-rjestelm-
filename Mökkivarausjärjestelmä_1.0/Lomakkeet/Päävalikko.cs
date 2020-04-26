6d4409779d12ffb8dfa744a336b0f650a2de48e9
Nicolas Kuusela <Nicolas.Kuusela@edu.savonia.fi>
2020-04-26 14:47:55 +0300

    DataModel päivitetty, Yhteydet luotu

---
+++ Mökkivarausjärjestelmä_1.0/Lomakkeet/Päävalikko.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mökkivarausjärjestelmä_1._0.Lomakkeet
+{
+    public partial class Päävalikko : Form
+    {
+        public Päävalikko()
+        {
+            InitializeComponent();
+        }
+    }
+}
