a9b0fc8682aecbd6fee21df950d7a17bd58b9b59
Mikko Kumpulainen <Mikko.Kumpulainen2@edu.savonia.fi>
2020-04-27 13:54:25 +0300

    Data Source lisätty

--- Mökkivarausjärjestelmä_1.0/Properties/Settings.Designer.cs
+++ Mökkivarausjärjestelmä_1.0/Properties/Settings.Designer.cs
@@ -33,5 +33,15 @@ namespace Mökkivarausjärjestelmä_1._0.Properties {
                 return ((string)(this["VillageNewbiesConnection"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Dsn=VillageNewbies;uid=root")]
+        public string ConnectionString {
+            get {
+                return ((string)(this["ConnectionString"]));
+            }
+        }
     }
 }
